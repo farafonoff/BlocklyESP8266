ec8ed411349008304c90650e4674430a90d91fca
farafonoff <farafonoff@gmail.com>
2018-11-24 17:10:21 +0400

    fix issue car2

--- Board/udp.lua
+++ Board/udp.lua
@@ -4,8 +4,10 @@ function listen_udp(port)
     udpSocket:on("receive", function(s, data, port, ip)
         if data ~= "discover" then
             processJsonString(data)
-            print(last_distance)
-            udpSocket:send(port, ip, string.format("distance:%f",last_distance));
+            if (last_distance) then
+                print(last_distance)
+                udpSocket:send(port, ip, string.format("distance:%f",last_distance));
+            end
         else
             print(string.format("discover: %s:%d", ip, port))    
             udpSocket:send(port, ip, data);
